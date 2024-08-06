692d2eab45bd61a227190c61c6f9fb3ae957bec1
ahibis <nikitonvg@list.ru>
2024-08-06 23:36:39 +0300

    feat: add background

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Application, Assets, Sprite } from 'pixijs';
+
+const app = new Application();
+app.view.width = window.innerWidth
+app.view.height = window.innerHeight
+
+app.screen.width = 1366
+app.screen.height = 768
+
+document.body.appendChild(app.view);
+
+const texture = await Assets.load('background/background1.png');
+
+const background = new Sprite(texture);
+
+// Add the bunny to the scene we are building
+app.stage.addChild(background);
