f30652dc481ba661eb819f7ba5164d318be21833
ahibis <nikitonvg@list.ru>
2024-08-09 02:31:33 +0300

    feat: add onUpdate and OnKeyDown to ObjectGroup

--- src/rooms/Room.ts
+++ src/rooms/Room.ts
@@ -1,12 +1,12 @@
-import { Application, Container, Ticker } from "pixi.js";
+import { Application, Container } from "pixi.js";
 import ObjectGroup from "@/objects/ObjectGroup";
 import GameObject from "@/objects/GameObject";
 export default class Room {
   app: Application;
   objects: GameObject[] = [];
   container = new Container();
-  updatableObjects: GameObject[] = [];
-  keyDownObjects: GameObject[] = [];
+  updatableObjects: (GameObject | ObjectGroup)[] = [];
+  keyDownObjects: (GameObject | ObjectGroup)[] = [];
   async addObject(object: GameObject) {
     await object.register(this);
     this.objects.push(object);
@@ -16,11 +16,16 @@ export default class Room {
     if ("onUpdate" in object) {
       this.updatableObjects.push(object);
     }
-
     this.container.addChild(object.sprite);
   }
   addObjectGroup(objectGroup: ObjectGroup) {
     this.container.addChild(objectGroup.container);
+    if ("onKeyDown" in objectGroup) {
+      this.keyDownObjects.push(objectGroup);
+    }
+    if ("onUpdate" in objectGroup) {
+      this.updatableObjects.push(objectGroup);
+    }
   }
 
   constructor(app: Application) {
@@ -28,7 +33,7 @@ export default class Room {
     this.container.sortableChildren = true;
     this.app.stage.addChild(this.container);
     this.app.ticker.deltaMS = 1000 / 60;
-    this.app.ticker.add((ticker: Ticker) => {
+    this.app.ticker.add((ticker) => {
       this.updatableObjects.forEach((object) => {
         if ("onUpdate" in object) {
           (object.onUpdate as Function)(ticker);
@@ -41,6 +46,6 @@ export default class Room {
           (object.onKeyDown as Function)(e);
         }
       });
-    })
+    });
   }
 }
